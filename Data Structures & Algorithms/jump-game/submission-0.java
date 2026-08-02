53a470587cb449fe783dbac42a435334764eccbd
jtanwar9 <99406120+jtanwar9@users.noreply.github.com>
2026-08-02 14:22:44 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(nums[i] + i >= reach) {
+                reach = i;
+            }
+        }
+        return reach == 0;
+    }
+}
