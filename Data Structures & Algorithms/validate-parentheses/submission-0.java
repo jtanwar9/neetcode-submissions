ee8e5fe007858d84cfec6330251d6a2b41089fee
jtanwar9 <99406120+jtanwar9@users.noreply.github.com>
2026-08-06 03:19:11 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        for(char ch : s.toCharArray()) {
+            if(map.containsKey(ch)) {
+                if(stack.isEmpty() || stack.pop() != map.get(ch)) {
+                    return false;
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
